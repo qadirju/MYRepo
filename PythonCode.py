e32b45db8ff07d56ecb96bf34c9d3f0718a35240
Ghulam Qadir <qadirju12@gmail.com>
2025-03-06 00:38:37 +0500

    Some Python basic files

---
+++ PythonCode.py
@@ -0,0 +1,2 @@
+print("This is me Ghulam Qadir")
+print("I am a student of BS Computer Science")
---
+++ PythonProject.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns   # for data visualization
+from sklearn.model_selection import train_test_split    # for splitting the data
+from sklearn.linear_model import LinearRegression    # for linear regression model
+from sklearn.metrics import mean_squared_error    # for mean squared error  calculation
+from sklearn.metrics import r2_score    # for r2 score calculation          # for data visualization
+from sklearn.model_selection import train_test_split    # for splitting the data
