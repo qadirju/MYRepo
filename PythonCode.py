ce499ffca85ff7ea79200c5a731887b74ae093fe
Ghulam Qadir <qadirju12@gmail.com>
2025-03-06 23:11:42 +0500

    Some Numpy function in Feature1 branch

--- PythonCode.py
+++ PythonCode.py
@@ -1,2 +1,14 @@
+import numpy as np
+
 print("This is me Ghulam Qadir")
 print("I am a student of BS Computer Science")
+
+# Example attributes of numpy
+print("Numpy version:", np.__version__)
+print("Numpy configuration:")
+np.show_config()
+
+# Additional numpy attributes
+# print("Numpy built with:", np.__built_with__)
+# print("Numpy license:", np.__license__)
+print("Numpy file:", np.__file__)
